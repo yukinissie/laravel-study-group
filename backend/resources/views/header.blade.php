22f6e08767683853cbf02d4b7c9ec85f8da78145
take-cantik <miki.takeru508@mail.kyutech.jp>
2022-01-13 14:59:19 +0900

    Add: default html and header

---
+++ backend/resources/views/header.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('header')
+<nav>
+  <a href="#">
+    Movies
+  </a>
+</nav>
+@endsection
---
+++ backend/resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>Laravel-Movies</title>
+  </head>
+  <body>
+    <div>
+      @yield('header')
+      @yield('content')
+    </div>
+  </body>
+</html>
