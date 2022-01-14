005735b39b0ad2673f172f5968b9beedf7d5e1a4
take-cantik <miki.takeru508@mail.kyutech.jp>
2022-01-14 22:04:15 +0900

    Update: edit style

--- backend/resources/views/admin/movies/edit.blade.php
+++ backend/resources/views/admin/movies/edit.blade.php
@@ -1,8 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-  <h1>Edit</h1>
-  {!! Form::model($movie, ['method'=>'put', 'route' => ['admin.movies.update', 'movie' => $movie->id]]) !!}
-    @include('admin.movies.form')
-  {!! Form::close() !!}
+  <div class="w-100 d-flex flex-column gap-4">
+    <h1>Edit</h1>
+    <div class="w-100 d-flex justify-content-center align-items-center">
+      {!!
+        Form::model(
+          $movie,
+          [
+            'method'=>'put',
+            'route' => ['admin.movies.update', 'movie' => $movie->id],
+            'route' => 'admin.movies.store',
+            'class' => 'w-100 d-flex flex-column gap-3',
+            'style' => 'max-width: 400px;'
+          ]
+        )
+      !!}
+        @include('admin.movies.form')
+      {!! Form::close() !!}
+    </div>
+  </div>
 @endsection
