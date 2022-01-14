cfa8f52b61f818005c8e1e520d04d1db4ce1e4c6
take-cantik <miki.takeru508@mail.kyutech.jp>
2022-01-14 16:29:32 +0900

    Add: style and script to view

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -2,13 +2,15 @@
 <html lang="ja">
   <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.css') }}" ></script>
     <title>Laravel-Movies</title>
   </head>
   <body>
     <header>
       @include('header')
     </header>
-    <main class=".p-120">
+    <main class="p-1">
       @yield('content')
     </main>
   </body>
