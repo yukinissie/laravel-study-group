6a5b4a888628556cce308b6536bc81aa7ab15ec2
take-cantik <take.cantik17@gmail.com>
2022-01-29 22:15:48 +0900

    Update: add type of presenter arg

--- backend/app/Http/Presenters/Movie/IndexPresenter.php
+++ backend/app/Http/Presenters/Movie/IndexPresenter.php
@@ -4,16 +4,19 @@ declare(strict_types=1);
 
 namespace App\Http\Presenters\Movie;
 
+use App\Http\Entities\Movie\MovieList;
+use App\Http\Entities\Movie\Movie;
+
 class IndexPresenter
 {
-    public function output($movies)
+    public function output(MovieList $movieList)
     {
-        return = array_map(function ($movie) {
+        return array_map(function (Movie $movie) {
             return [
-                'id' => $movie->id,
-                'title' => $movie->title,
-                'imageUrl' => $movie->imageUrl
+                'id' => $movie->getId(),
+                'title' => $movie->getTitle(),
+                'imageUrl' => $movie->getImageUrl()
             ];
-        });
+        }, $movieList->getList());
     }
 }
