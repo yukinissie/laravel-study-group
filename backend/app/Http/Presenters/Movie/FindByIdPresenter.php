7b9f8a332cb5cee1269ade0f16ac80b8b6bdb8ad
take-cantik <take.cantik17@gmail.com>
2022-01-29 22:21:47 +0900

    Add: findbyid presenter

---
+++ backend/app/Http/Presenters/Movie/FindByIdPresenter.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Presenters\Movie;
+
+use App\Http\Entities\Movie\Movie;
+
+class FindByIdPresenter
+{
+    public function output(Movie $movie)
+    {
+        return [
+            'id' => $movie->getId(),
+            'title' => $movie->getTitle(),
+            'imageUrl' => $movie->getImageUrl()
+        ];
+    }
+}
